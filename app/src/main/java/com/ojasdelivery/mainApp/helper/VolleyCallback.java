e9efc898ccfc85cb5627a48338e4f01d04d1d916
sharma2rachit <73378516+sharma2rachit@users.noreply.github.com>
2022-05-01 15:55:04 +0530

    name changes to RJ

--- app/src/main/java/com/ojasdelivery/mainApp/helper/VolleyCallback.java
+++
@@ -1,8 +0,0 @@
-package com.ojasdelivery.mainApp.helper;
-
-public interface VolleyCallback {
-    void onSuccess(boolean result, String message);
-
-    //void onSuccessWithMsg(boolean result, String message);
-
-}
